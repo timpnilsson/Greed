72181f31524dfe653214a18449f29c68c1eacee9
Tim Nilsson <Tim@Tims-MacBook-Pro-2.local>
2015-06-26 10:10:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tim/greed/ImageAdapter.java
@@ -0,0 +1,61 @@
+package com.example.tim.greed;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.red1,
+            R.drawable.red2,
+            R.drawable.red3,
+            R.drawable.red4,
+            R.drawable.red5,
+            R.drawable.red6,
+            R.drawable.test,
+            R.drawable.test,
+            R.drawable.test
+
+
+    };
+}
